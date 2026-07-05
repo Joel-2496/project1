90b0a97cd17314c70716cbba607884e12936cb35
Joel-2496 <piresjoelgoa@gmail.com>
2026-07-05 13:56:14 +0400

    Update Program3.py

--- Mysqlconnect/Program3.py
+++ Mysqlconnect/Program3.py
@@ -7,8 +7,8 @@ def Program3():
         host="localhost",
         port=3306,
         user="root",
-        password="2496",
-        database="python_video"
+        password="",
+        database=""
     )
 
     mycursor = mydb.cursor()
